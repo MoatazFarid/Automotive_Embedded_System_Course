97e8c95bb781990b9230de6ceb109c41e5aa4fca
Moataz Farid <MoatazMFarid@gmail.com>
2018-12-21 09:32:07 +0200

    Update lab5.c

--- 01_C_programming/Lec_05_Labs/lab5.c
+++ 01_C_programming/Lec_05_Labs/lab5.c
@@ -13,3 +13,67 @@ Searching Algorithm.*/
 // 1- sort the array 1st 
 // 2- if n/2 (n is the size of the array ) < the requested no 
 // 4- then if (n/2)/2 
+
+#include<stdio.h>
+/************************************************************************/
+/* Name: Binary Search function											*/
+/* function: used to find a certain value val from array arr			*/
+/* Inputs   :1-int *arr -> pointer to array element that array should	*/
+/* 			               be sorted array 								*/
+/* 			2-int size -> size of that array 							*/
+/*			3-int val -> value to search for in the array 				*/
+/* output   :-1 if not found and index of it if found					*/
+/************************************************************************/
+int binarySearch(int *arr , int size , int val){
+	int found = 0,
+		high = size-1,
+		low =0,
+		m = (high/2)+1;
+	while(!found){
+		if((high == low)){
+			if(val == arr[m]){	//case arr={1} and size =1 and val =1 
+				return m;
+			}else if(!found){
+				break; // no value was found in the array 
+			}
+			
+		}else if(val == arr[m]){ //found
+			found=1;
+		}else if(val < arr[m]){
+			high=m;
+			m=(high-low)/2;
+			
+		}else if(val > arr[m]){
+			low=m+1;
+			m=((high - low)/2)+low;
+		}else{
+			
+		}
+		
+	}
+	
+	if(found==1){
+		return m;
+	}else{
+		return -1;
+	}
+}
+void main(void){
+	int arr[6]={0,1,2,3,4,5};
+	//test cases 
+	//case -1 
+	printf("index (-1)= %d\n",binarySearch(arr,6,-1));
+	//case -10
+	printf("index (-10)= %d\n",binarySearch(arr,6,-10));
+	// case 10 
+	printf("index (10)= %d\n",binarySearch(arr,6,10));
+	//case 0
+	printf("index (0)= %d\n",binarySearch(arr,6,0));
+	
+	printf("index = %d\n",binarySearch(arr,6,0));
+	printf("index = %d\n",binarySearch(arr,6,1));
+	printf("index = %d\n",binarySearch(arr,6,2));
+	printf("index = %d\n",binarySearch(arr,6,3));
+	printf("index = %d\n",binarySearch(arr,6,4));
+	printf("index = %d\n",binarySearch(arr,6,5));
+}
